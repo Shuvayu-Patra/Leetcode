fe1574365408ede4415f7e6b9f549d82cdde340d
Shuvayu-Patra <shuvayupatra@gmail.com>
2023-03-22 23:23:18 +0530

    Time: 2176 ms (8.40%), Space: 31 MB (93.93%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,15 +1,24 @@
 class Solution {
-    int leftsum(int a , vector<int>&nums){
-        int sum = 0;
-        for(int i=0 ; i<a ; i++){
-            sum += nums[i];
-        }
-        return sum;
-    }
+//     int leftsum(int a , vector<int>&nums){
+//         int sum = 0;
+//         for(int i=0 ; i<a ; i++){
+//             sum += nums[i];
+//         }
+//         return sum;
+//     }
     
-    int rightsum(int a , vector<int>nums){
-        int sum = 0;
-        for(int i=a+1 ; i<nums.size() ; i++){
+//     int rightsum(int a , vector<int>nums){
+//         int sum = 0;
+//         for(int i=a+1 ; i<nums.size() ; i++){
+//             sum += nums[i];
+//         }
+//         return sum;
+//     }
+    
+    
+    int prefixsum(int a , vector<int>& nums,int b){
+        int sum = 0; 
+        for(int i=a ; i<b ; i++){
             sum += nums[i];
         }
         return sum;
@@ -18,8 +27,13 @@ public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
         for(int i=0 ; i<n ; i++){
-            int s = leftsum(i,nums);
-            int H = rightsum(i,nums);
+        //     int s = leftsum(i,nums);
+        //     int H = rightsum(i,nums);
+        //     if(s == H) return i;
+        // }
+            
+            int s = prefixsum(0,nums,i);
+            int H = prefixsum(i+1,nums,n);
             if(s == H) return i;
         }
         return -1;
