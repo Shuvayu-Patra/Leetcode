59b9e276132ec195b20ff33a3f170da7f8f18093
Shuvayu-Patra <shuvayupatra@gmail.com>
2023-04-14 00:13:02 +0530

    Time: 312 ms (87.26%), Space: 94.5 MB (61.40%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
@@ -11,25 +11,38 @@
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-        ListNode* temp = list1;
-        ListNode* temp1 = list1;
-        int pos = 1;
-        while(pos < a){
-            temp = temp -> next;
-            pos++;
+        // ListNode* temp = list1;
+        // ListNode* temp1 = list1;
+        // int pos = 1;
+        // while(pos < a){
+        //     temp = temp -> next;
+        //     pos++;
+        // }
+        // pos = 0;
+        // while(pos < b){
+        //     temp1 = temp1 -> next;
+        //     pos++;
+        // }
+        // ListNode* curr = list2;
+        // temp -> next = curr;
+        // while(curr -> next != NULL){
+        //     curr = curr -> next;
+        // }
+        // curr -> next = temp1 -> next;
+        // return list1; 
+        
+        a--;
+        b++;
+        ListNode* tail = list2;
+        while(tail->next != NULL){
+            tail = tail->next;
         }
-        pos = 0;
-        while(pos < b){
-            temp1 = temp1 -> next;
-            pos++;
-        }
-        ListNode* curr = list2;
-        temp -> next = curr;
-        while(curr -> next != NULL){
-            curr = curr -> next;
-        }
-        curr -> next = temp1 -> next;
-        //temp1 -> next = NULL;
-        return list1;  
+        ListNode* h1 = list1;
+        ListNode* t1 = list1;
+        while(a--) h1 = h1->next;
+        while(b--) t1 = t1->next;
+        h1->next = list2;
+        tail->next = t1;
+        return list1;
     }
 };
