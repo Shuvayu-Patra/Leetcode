f4a6588ab5bf6e323a739cac5719bf938e5939b8
Shuvayu-Patra <shuvayupatra@gmail.com>
2023-04-14 01:36:26 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (56.64%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    char shift(char c , int x){
-        int a = x-'0';
-        return c+a;
-    }
-    
     string replaceDigits(string s) {
-        string ans = "";
-        for(int i=0 ; i<s.size() ; i++){
-            if(i&1){
-                ans.push_back(shift(s[i-1], s[i]));
-            }
-            else ans.push_back(s[i]);
+        for(int i=1 ; i<s.size() ; i+=2){
+            s[i] += s[i-1] - '0';
         }
-        return ans;
+        return s;
     }
 };
