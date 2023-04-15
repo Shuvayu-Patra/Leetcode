19d016f024a83ae718e76c1713b903fb60d87527
Shuvayu-Patra <shuvayupatra@gmail.com>
2023-04-16 01:55:07 +0530

    Time: 5 ms (15.32%), Space: 6.6 MB (5.95%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    bool backspaceCompare(string S, string T) {
-//         stack<int>s1,s2;
-//         string str1 , str2;
+    bool backspaceCompare(string s, string t) {
+        stack<char>s1,s2;
+        //string str1 , str2;
         
-//         for(int i=0 ; i<s.size() ; i++){
-//             if(s[i] == '#' && !s1.empty()) s1.pop();
+        for(int i=0 ; i<s.size() ; i++){
+            if(s[i] == '#' && !s1.empty()) s1.pop();
             
-//             else if(s[i] != '#') s1.push(s[i]);
-//         }
+            else if(s[i] != '#') s1.push(s[i]);
+        }
         
-//         for(int i=0 ; i<t.size() ; i++){
-//             if(t[i] == '#' && !s2.empty()) s2.pop();
+        for(int i=0 ; i<t.size() ; i++){
+            if(t[i] == '#' && !s2.empty()) s2.pop();
             
-//             else if(t[i] != '#') s2.push(s[i]);
-//         }
+            else if(t[i] != '#') s2.push(t[i]);
+        }
         
+        return s1==s2;
 //         while(!s1.empty()){
                  
 //                  str1.push_back(s1.top());
@@ -32,43 +33,43 @@ public:
             
 //          return str1 == str2;
         
-        stack <int> s1,s2;
-         string str1,str2; 
+//         stack <int> s1,s2;
+//          string str1,str2; 
             
-         for(int i = 0; i < S.size(); i++){
+//          for(int i = 0; i < S.size(); i++){
                  
-                 if(S[i] == '#' && !s1.empty())
-                         s1.pop();
+//                  if(S[i] == '#' && !s1.empty())
+//                          s1.pop();
                  
-                 else if(S[i] != '#')
-                         s1.push(S[i]);
+//                  else if(S[i] != '#')
+//                          s1.push(S[i]);
                  
-         }
+//          }
          
-         for(int i = 0; i < T.size(); i++){
+//          for(int i = 0; i < T.size(); i++){
                  
-                 if(T[i] == '#' && !s2.empty())
-                         s2.pop();
+//                  if(T[i] == '#' && !s2.empty())
+//                          s2.pop();
                  
-                 else if(T[i] != '#')
-                         s2.push(T[i]);
+//                  else if(T[i] != '#')
+//                          s2.push(T[i]);
                  
-         }
+//          }
             
-         while(!s1.empty()){
+//          while(!s1.empty()){
                  
-                 str1.push_back(s1.top());
-                 s1.pop();
+//                  str1.push_back(s1.top());
+//                  s1.pop();
                  
-         }
+//          }
             
-         while(!s2.empty()){
+//          while(!s2.empty()){
                  
-                 str2.push_back(s2.top());
-                 s2.pop();
+//                  str2.push_back(s2.top());
+//                  s2.pop();
                  
-         }      
+//          }      
             
-         return str1 == str2;   
+//          return str1 == str2;   
     }
 };
