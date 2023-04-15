6b5aec41c61737aa25bb1b4329cf69c760cbc450
Shuvayu-Patra <shuvayupatra@gmail.com>
2023-04-16 01:57:32 +0530

    Time: 4 ms (36.69%), Space: 6.4 MB (32.35%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t) {
         stack<char>s1,s2;
-        //string str1 , str2;
         
         for(int i=0 ; i<s.size() ; i++){
             if(s[i] == '#' && !s1.empty()) s1.pop();
@@ -16,60 +15,6 @@ public:
             else if(t[i] != '#') s2.push(t[i]);
         }
         
-        return s1==s2;
-//         while(!s1.empty()){
-                 
-//                  str1.push_back(s1.top());
-//                  s1.pop();
-                 
-//          }
-            
-//          while(!s2.empty()){
-                 
-//                  str2.push_back(s2.top());
-//                  s2.pop();
-                 
-//          }      
-            
-//          return str1 == str2;
-        
-//         stack <int> s1,s2;
-//          string str1,str2; 
-            
-//          for(int i = 0; i < S.size(); i++){
-                 
-//                  if(S[i] == '#' && !s1.empty())
-//                          s1.pop();
-                 
-//                  else if(S[i] != '#')
-//                          s1.push(S[i]);
-                 
-//          }
-         
-//          for(int i = 0; i < T.size(); i++){
-                 
-//                  if(T[i] == '#' && !s2.empty())
-//                          s2.pop();
-                 
-//                  else if(T[i] != '#')
-//                          s2.push(T[i]);
-                 
-//          }
-            
-//          while(!s1.empty()){
-                 
-//                  str1.push_back(s1.top());
-//                  s1.pop();
-                 
-//          }
-            
-//          while(!s2.empty()){
-                 
-//                  str2.push_back(s2.top());
-//                  s2.pop();
-                 
-//          }      
-            
-//          return str1 == str2;   
+        return s1==s2;   
     }
 };
