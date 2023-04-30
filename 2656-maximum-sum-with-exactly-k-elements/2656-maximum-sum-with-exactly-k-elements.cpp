c7f583811be77ca5798409572a98b0eb86a4076e
Shuvayu-Patra <shuvayupatra@gmail.com>
2023-04-30 21:20:17 +0530

    Time: 59 ms (22.22%), Space: 70.7 MB (33.33%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int maximizeSum(vector<int>& nums, int k) {
-        int ans=0;
         sort(nums.begin() , nums.end());
-        while(k!=0){
-            int a = nums[nums.size()-1];
-            ans+=a;
-            nums.pop_back();
-            nums.push_back(a+1);
-            k--;
+        int a = nums[nums.size()-1];
+        int ans = 0;
+        for(int i=1 ; i<=k ; i++){
+            ans +=  a++;
         }
         return ans;
+        
+        
+        // int ans=0;
+        // sort(nums.begin() , nums.end());
+        // while(k!=0){
+        //     int a = nums[nums.size()-1];
+        //     ans+=a;
+        //     nums.pop_back();
+        //     nums.push_back(a+1);
+        //     k--;
+        // }
+        // return ans;
     }
 };
