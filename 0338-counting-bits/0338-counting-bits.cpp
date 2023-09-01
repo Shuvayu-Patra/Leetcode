d1f17765a19a3238d80b932d4444b17f283a06d0
Shuvayu-Patra <shuvayupatra@gmail.com>
2023-09-01 10:56:32 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (65.80%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
-        for(int i=0; i<=n; i++){
-            int temp = i;
-            int set_bits = 0;
-            while(temp){
-            if((temp&1) == 1){
-                set_bits++;
-            }
-            temp = temp>>1;
-            }
-            ans.push_back(set_bits);
+        // vector<int>ans;
+        // for(int i=0; i<=n; i++){
+        //     int temp = i;
+        //     int set_bits = 0;
+        //     while(temp){
+        //     if((temp&1) == 1){
+        //         set_bits++;
+        //     }
+        //     temp = temp>>1;
+        //     }
+        //     ans.push_back(set_bits);
+        // }
+        // return ans;
+        
+        vector<int>t(n+1);
+        t[0]=0;
+        for(int i=1; i<=n; ++i){
+            t[i] = t[i/2] + (i%2);
         }
-        return ans;
+        return t;
     }
 };
