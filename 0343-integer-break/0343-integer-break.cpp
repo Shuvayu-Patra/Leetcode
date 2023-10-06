6b73ec78b7421e34973a575ebe9bcbed3ef482b4
Shuvayu-Patra <shuvayupatra@gmail.com>
2023-10-06 12:35:28 +0530

    Time: 2 ms (49.96%), Space: 6.6 MB (22.36%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        int dp[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int j=2;j<n+1;j++){
-            int maxi=0;
-            for(int i=1;i<j;i++){
-                maxi=max(maxi,max(dp[j-i]*i,i*(j-i)));
-            }
-            dp[j]=maxi;
-        }
-        return dp[n];
+        if (n <= 3) return n - 1;
+        int quotient = n / 3, remainder = n % 3;
+        if (remainder == 0) return pow(3, quotient);
+        if (remainder == 1) return pow(3, quotient - 1) * 4;
+        return pow(3, quotient) * 2;
     }
 };
