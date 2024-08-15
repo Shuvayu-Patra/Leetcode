d2217ba4c40530924466a04e6834aa545c7a244b
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-08-15 17:46:26 +0530

    Time: 13 ms (95.58%), Space: 94.8 MB (58.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        int n = (n1+n2);
-        int ind2 = (n/2);
-        int ind1 = (ind2 - 1);
-        int cnt = 0;
-        int i=0, j=0;
-        int ele1 = -1, ele2 = -1;
-        while(i < n1 && j < n2){
-            if(nums1[i] < nums2[j]){
-                if(cnt == ind1) ele1 = nums1[i];
-                if(cnt == ind2) ele2 = nums1[i];
-                cnt++;
-                i++;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int n = n1+n2;
+        if(n1 > n2) return findMedianSortedArrays(nums2,nums1);
+        int l = 0, h = n1;
+        int left = (n1+n2+1)/2;
+        while(l <= h){
+            int mid1 = (l + h)/2;
+            int mid2 = left - mid1;
+            int l1=INT_MIN, l2=INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1) return max(l1,l2);
+                else return ((double)(max(l1,l2) + min(r1,r2))) / 2.0;
             }
-            else{
-                if(cnt == ind1) ele1 = nums2[j];
-                if(cnt == ind2) ele2 = nums2[j];
-                cnt++;
-                j++;
-            }
+            else if(l1 > r2) h = mid1 - 1;
+            else l = mid1 + 1;
         }
-        
-        while(i < n1){
-                if(cnt == ind1) ele1 = nums1[i];
-                if(cnt == ind2) ele2 = nums1[i];
-                cnt++;
-                i++;
-        }
-        
-        while( j < n2){
-                if(cnt == ind1) ele1 = nums2[j];
-                if(cnt == ind2) ele2 = nums2[j];
-                cnt++;
-                j++;
-        }
-        
-        
-        if(n%2 == 1) return ele2;
-        return (double)((double)(ele1+ele2))/2.0;     
+        return 0;
     }
 };
