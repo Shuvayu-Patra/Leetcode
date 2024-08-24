eb93658c536b9f1d1df92ad68ab2200e1e40ce23
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-08-24 22:00:36 +0530

    Time: 10 ms (58.68%), Space: 14.3 MB (7.72%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,47 +2,18 @@ class Solution {
 public:
     int romanToInt(string s) {
         int ans = 0;
-        for(int i=s.size()-1; i>=0; i--){
-            if(i>0 and s[i] == 'V' and s[i-1] == 'I'){
-                ans+=4;
-                i--;
-            } 
-            else if(i>0 and s[i] == 'X' and s[i-1] == 'I'){
-                ans+=9;
-                i--;
-            } 
-            else if(i>0 and s[i] == 'L' and s[i-1] == 'X'){
-                ans+=40;
-                i--;
-            } 
-            else if(i>0 and s[i] == 'C' and s[i-1] == 'X'){
-                ans+=90;
-                i--;
-            } 
-            else if(i>0 and s[i] == 'D' and s[i-1] == 'C'){
-                ans+=400;
-                i--;
-            } 
-            else if(i>0 and s[i] == 'M' and s[i-1] == 'C'){
-                ans+=900;
-                i--;
-            }
-            else if(s[i] == 'I') ans+=1;
-            else if(s[i] == 'V') ans+=5;
-            else if(s[i] == 'X') ans+=10;
-            else if(s[i] == 'L') ans+=50;
-            else if(s[i] == 'C') ans+=100;
-            else if(s[i] == 'D') ans+=500;
-            else if(s[i] == 'M') ans+=1000;
+        unordered_map<char, int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        for(int i=0; i<s.size(); i++){
+            if(m[s[i]] < m[s[i+1]]) ans -= m[s[i]];
+            else ans += m[s[i]];
         }
-        // if(s.size() == 2) return ans;
-        // if(s[0] == 'I') ans += 1;
-        // else if(s[0] == 'V') ans+=5;
-        // else if(s[0] == 'X') ans+=10;
-        // else if(s[0] == 'L') ans+=50;
-        // else if(s[0] == 'C') ans+=100;
-        // else if(s[0] == 'D') ans+=500;
-        // else if(s[0] == 'M') ans+=1000;
         return ans;
     }
 };
