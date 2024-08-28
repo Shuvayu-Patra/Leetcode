d0d39649872d6ea70a89a2e859d611b50d44997a
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-08-28 21:46:57 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (54.37%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -10,7 +10,8 @@ public:
             int mid = (l+h)/2;
             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
             if(nums[mid] > nums[mid+1]) h = mid - 1;
-            else l = mid + 1;
+            else if(nums[mid] < nums[mid+1]) l = mid + 1;
+            else h = mid - 1;
         }
         return -1;
     }
