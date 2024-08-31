c8b4ea511f1f667f8dd5d86acf799127c4e437f0
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-08-31 12:39:39 +0530

    Time: 0 ms (100.00%), Space: 10 MB (8.03%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int func(vector<int>& v, int pages, int k){
-        int stuCnt = 1, pageHolding = 0;
+    int cntStu(vector<int>v, int maxP){
+        int cnt = 1, pageHold = 0;
         for(int i=0; i<v.size(); i++){
-            if((pageHolding + v[i]) <= pages){
-                pageHolding += v[i];
+            if((pageHold+v[i]) > maxP){
+                cnt++;
+                pageHold = v[i];
             }
-            else{
-                stuCnt++;
-                pageHolding = v[i];
-            }
+            else pageHold += v[i];
         }
-        return stuCnt;
+        return cnt;
     }
     
     int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
         int l = *max_element(nums.begin(), nums.end());
         int h = accumulate(nums.begin(), nums.end(), 0);
         while(l <= h){
-            int mid = l+(h-l)/2;
-            int stuCnt = func(nums,mid,k);
-            if(stuCnt <= k){
-                h = mid - 1;
-            }
-            else{
-                l = mid + 1;
-            }
+            int mid = (l+h)/2;
+            int cnt = cntStu(nums,mid);
+            if(cnt > k) l = mid + 1;
+            else h = mid - 1;
         }
         return l;
     }
