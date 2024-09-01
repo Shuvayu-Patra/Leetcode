84fb04f502369e0056367882bad9bfaa906706c4
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-09-01 11:32:26 +0530

    Time: 4 ms (22.22%), Space: 8 MB (38.89%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.cpp
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.cpp
@@ -5,26 +5,14 @@ public:
         if(((coordinate1[0] - 'a')%2 == 0) && (coordinate1[1] - '0')%2==1){
             ans1 = true;
         }
-        else if(((coordinate1[0] - 'a')%2 == 0) && (coordinate1[1] - '0')%2==0){
-            ans1 = false;
-        }
         else if(((coordinate1[0] - 'a')%2 == 1) && (coordinate1[1] - '0')%2==0){
             ans1 = true;
-        }
-        else if(((coordinate1[0] - 'a')%2 == 1) && (coordinate1[1] - '0')%2==1){
-            ans1 = false;
         }
         if(((coordinate2[0] - 'a')%2 == 0) && (coordinate2[1] - '0')%2==1){
             ans2 = true;
         }
-        else if(((coordinate2[0] - 'a')%2 == 0) && (coordinate2[1] - '0')%2==0){
-            ans2 = false;
-        }
         else if(((coordinate2[0] - 'a')%2 == 1) && (coordinate2[1] - '0')%2==0){
             ans2 = true;
-        }
-        else if(((coordinate2[0] - 'a')%2 == 1) && (coordinate2[1] - '0')%2==1){
-            ans2 = false;
         }
         return ans1 == ans2;
     }
