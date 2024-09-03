3ace8657f4deeb0ed782ecb9e27b33a47d02692a
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-09-03 21:48:01 +0530

    Time: 84 ms (89.79%), Space: 93 MB (6.11%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
+        int ans = 0;
         k = k%accumulate(chalk.begin(), chalk.end(), 0l);
-        long long sum = 0;
+        vector<long long>v;
+        long long pSum = 0;
         for(int i=0; i<chalk.size(); i++){
-            sum += chalk[i];
-            if(sum > k){
-                return i;
-                break;
-            }
+            pSum += chalk[i];
+            v.push_back(pSum);
         }
-        return 0;
+        int l=0, h = v.size()-1;
+        while(l <= h){
+            int mid = l+(h-l)/2;
+            if(v[mid] > k){
+                ans = mid;
+                h = mid - 1;
+            }
+            else l = mid + 1;
+        }
+        return ans;
     }
 };
