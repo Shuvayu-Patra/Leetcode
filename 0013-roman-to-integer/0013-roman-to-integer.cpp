afb1f64e7716d92fdc49af5e1b678a547a9f8afe
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-09-03 16:34:53 +0530

    Time: 17 ms (18.06%), Space: 14.5 MB (5.07%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         int ans = 0;
-        unordered_map<char, int> m;
+        map<char, int>m;
         m['I'] = 1;
         m['V'] = 5;
         m['X'] = 10;
@@ -10,9 +10,12 @@ public:
         m['C'] = 100;
         m['D'] = 500;
         m['M'] = 1000;
+        
         for(int i=0; i<s.size(); i++){
-            if(m[s[i]] < m[s[i+1]]) ans -= m[s[i]];
-            else ans += m[s[i]];
+            if(m[s[i]] < m[s[i+1]]){
+                ans -= m[s[i]];
+            }
+            else ans+=m[s[i]];
         }
         return ans;
     }
