4197e43b6e576b24deb667d686b02f10a31bca0c
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-09-08 13:26:19 +0530

    Time: 211 ms (20.00%), Space: 171.5 MB (30.00%) - LeetHub

--- 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
@@ -5,12 +5,10 @@ public:
         long long n = nums.size()-1;
         long long j = 0;
         for(int i=1; i<nums.size(); i++){
-            // int maxi = nums[j];
             if((j<i) && (nums[i] > nums[j])){
                 sum += ((i-j) * nums[j]);
                 j = i;
             }
-            // else if( i==nums.size()-1) sum += ((nums.size() - 1) - j) * nums[j];
         }
         sum += (n - j)*nums[j];
         return sum;
