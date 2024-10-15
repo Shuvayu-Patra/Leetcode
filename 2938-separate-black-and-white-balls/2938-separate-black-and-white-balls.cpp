72e7f584658076bc82d9f6f68e0d9af4c26e9925
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-10-15 13:39:54 +0530

    Time: 34 ms (48.42%), Space: 15.6 MB (76.42%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -3,7 +3,7 @@ public:
     long long minimumSteps(string s) {
         long long ans = 0, cnt=0;
         for(int i=s.length()-1; i>=0; i--){
-            if(s[i] == '0') cnt++;
+            if(s.at(i) == '0') cnt++;
             else ans += cnt;
         }
         return ans;
