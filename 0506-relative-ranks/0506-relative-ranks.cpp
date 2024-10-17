2b254bca97fe9119c3d5e3e0d3664bb13d5e5875
Shuvayu-Patra <shuvayupatra@gmail.com>
2024-10-17 13:47:45 +0530

    Time: 8 ms (69.90%), Space: 17.7 MB (8.25%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        vector<string>ans(score.size());
-        priority_queue<pair<int,int>>pq;
-        for(int i=0; i<score.size(); i++){
-            pq.push({score[i], i});
+        vector<string>ans;
+        vector<int>v = score;
+        sort(v.begin(), v.end(), greater<int>());
+        vector<string>medals = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        unordered_map<int, string>m;
+        for(int i=0; i<v.size(); i++){
+            if(i < 3) m[v[i]] = medals[i];
+            else m[v[i]] = to_string(i+1);
         }
-        
-        // while(!pq.empty()){
-        //     cout << pq.top().first << " ";
-        //     cout << pq.top().second << " ";
-        //     pq.pop();
-        // }
-        
-        int place = 1;
-        while(!pq.empty()){
-            int oInd = pq.top().second;
-            pq.pop();
-            if(place == 1) ans[oInd] = "Gold Medal";
-            else if(place == 2) ans[oInd] = "Silver Medal";
-            else if(place == 3) ans[oInd] = "Bronze Medal";
-            else ans[oInd] = to_string(place);
-            place++;
-        }
+        for(auto it : score) ans.push_back(m[it]);
         return ans;
     }
 };
